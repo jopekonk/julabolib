da36de3dd74043426437e49f84b1174fbf7bfb80
Joonas Konki <joonas.konki@gmail.com>
2019-01-10 15:05:50 +0100

    Added an example script to read status from a unit.

---
+++ example1_read_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Example script to read the status from the julabo unit
+# 20190110 - Joonas Konki
+
+import julabolib
+import time
+
+mychiller = julabolib.JULABO('/dev/ttyUSB2', baud=4800)
+
+val = mychiller.get_status()
+print("Julabo status is: " + val)
+
+val = mychiller.get_version()
+print("Julabo version is: " + val)
+
+val = mychiller.get_work_temperature()
+print("Julabo working temperature is: " + str(val) )
+
+val = mychiller.get_temperature()
+print("Julabo actual bath temperature is: " + str(val) )
+
+mychiller.close()
